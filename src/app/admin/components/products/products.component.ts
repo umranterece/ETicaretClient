bca69dd3ae2c732b712845680e2ca803cea262e5
Umran Terece <chost609@gmail.com>
2025-04-13 14:26:22 +0300

    ###

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Create_Product } from 'src/app/contratcs/create_product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -18,39 +19,7 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
 
-    // LISTELEME
-    // this.httpClientService.get({
-    //   controller:"products"
-    // }).subscribe(data=>console.log(data));
 
-
-    // EKLEME 
-    // this.httpClientService.post({
-    //   controller:"products"
-    // },{
-    //   name:"Pensan",
-    //   stock:100,
-    //   price:120
-    // }).subscribe(data=>console.log(data));
-
-    // GUNCELLEME
-    // this.httpClientService.put({
-    //   controller:"products"
-    // },{
-    //   Id:"2a3fca6d-53cb-4335-8271-8052f0c5edf0",
-    //   Name:"Pensen Ismi Guncellendi olan",
-    //   Stock:101,
-    //   Price:12
-    // }).subscribe();
-
-    // SILME
-    // this.httpClientService.delete({
-    //   controller:"products"
-    // },"47521b3a-e80e-4a6d-b023-ed43eda6714f").subscribe();
-
-    // this.httpClientService.get({
-    //   fullEndPoint:"https://jsonplaceholder.typicode.com/posts"
-    // }).subscribe(data=>console.log(data));
 
   }
 }
