fb3ed25d868304dcabace825e9a0adca944942d6
Umran Terece (MacBook) <chost609@gmail.com>
2025-04-19 02:17:47 +0300

    Delete Modal Dialog

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 declare var $:any;
@@ -13,7 +15,8 @@ export class DeleteDirective  {
   constructor(private element:ElementRef, 
     private _renderer:Renderer2,
     private productService:ProductService,
-    private spinner:NgxSpinnerService) { 
+    private spinner:NgxSpinnerService,
+    public dialog:MatDialog) { 
 
 
       const img = this._renderer.createElement('img');
@@ -30,12 +33,33 @@ export class DeleteDirective  {
 
     @HostListener("click")
     async onclick(){
-      this.spinner.show(SpinnerType.BallAtom);
-      console.log(this.id);
-      const td:HTMLTableCellElement=this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).fadeOut(500,()=>{
-        this.callback.emit();
+      this.openDialog(async ()=>{
+        this.spinner.show(SpinnerType.BallAtom);
+        console.log(this.id);
+        const td:HTMLTableCellElement=this.element.nativeElement;
+        await this.productService.delete(this.id);
+        $(td.parentElement).animate({
+          opacity:0,
+          left:"+=50",
+          height:"toggle"
+        },500,()=>{
+          this.callback.emit();
+        });
+        
+      })
+    }
+
+    openDialog(afterClosed:any): void {
+      const dialogRef = this.dialog.open(DeleteDialogComponent, {
+        width:"250px",
+        data: DeleteState.Yes,
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        if (result == DeleteState.Yes) {
+          console.log('The dialog was closed');
+          afterClosed();
+        }
       });
     }
     
