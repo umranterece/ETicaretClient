5a98b97615127a73b4bcf8a9aa2ccd6f81a1ebfb
Umran Terece (MacMini) <chost609@gmail.com>
2025-05-05 01:35:54 +0300

    Identity Mekanizmasinin Kurulmasi

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Create_User } from 'src/app/contratcs/users/create_user';
 import { User } from 'src/app/entities/user';
+import { UserService } from 'src/app/services/common/models/user.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
@@ -8,19 +11,22 @@ import { User } from 'src/app/entities/user';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, 
+    private userService:UserService,
+    private toastrService:CustomToastrService
+  ) {
 
   }
 
   frm: FormGroup;
   ngOnInit(): void {
     this.frm = this.formBuilder.group({
-      adSoyad: ["", [
+      nameSurname: ["", [
         Validators.required,
         Validators.maxLength(50),
         Validators.minLength(3)
       ]],
-      kullaniciAdi: ["", [
+      username: ["", [
         Validators.required,
         Validators.maxLength(50),
         Validators.minLength(3)
@@ -30,18 +36,18 @@ export class RegisterComponent implements OnInit {
         Validators.maxLength(250),
         Validators.email
       ]],
-      sifre: ["",
+      password: ["",
         [
           Validators.required
         ]],
-      sifreTekrar: ["",
+      passwordConfirm: ["",
         [
           Validators.required
         ]]
     }, {
       validators: (group: AbstractControl): ValidationErrors | null => {
-        let sifre = group.get("sifre").value;
-        let sifreTekrar = group.get("sifreTekrar").value;
+        let sifre = group.get("password").value;
+        let sifreTekrar = group.get("passwordConfirm").value;
         return sifre === sifreTekrar ? null : { notSame: true };
       }
     })
@@ -53,10 +59,24 @@ export class RegisterComponent implements OnInit {
 
   submitted: boolean = false;
 
-  onSubmit(data: User) {
+   async onSubmit(user: User) {
     this.submitted = true;
     if (this.frm.invalid)
       return
+
+    const result:Create_User=  await this.userService.create(user);
+    if(result.succeeded){
+      this.toastrService.message(result.message,"Kullanici Kaydi Basarili",{
+        messageType:ToastrMessageType.Success,
+        position:ToastrPosition.TopRight
+      })
+    }
+    else{
+      this.toastrService.message(result.message,"Hata",{
+        messageType:ToastrMessageType.Error,
+        position:ToastrPosition.TopRight
+      })
+    }
   }
 
 }
